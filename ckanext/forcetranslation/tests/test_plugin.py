bfcc41ec5c595942388c6b0fa636f4827cae388c
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-08-30 09:39:53 +0300

    Added basic unit tests

--- ckanext/forcetranslation/tests/test_plugin.py
+++ ckanext/forcetranslation/tests/test_plugin.py
@@ -1,5 +1,26 @@
 """Tests for plugin.py."""
 import ckanext.forcetranslation.plugin as plugin
+import os
+
 
 def test_plugin():
-    pass
+    p = plugin.ForceTranslationPlugin()
+
+    module_name = 'ckanext.forcetranslation'
+    i18n_path = 'i18n'
+    domain = 'test_domain'
+
+    p.configure({
+        'ckanext.forcetranslation.module': module_name,
+        'ckanext.forcetranslation.path': i18n_path,
+        'ckanext.forcetranslation.domain': domain
+        })
+
+    path, should_be_i18n_path = os.path.split(p.i18n_directory())
+    path, should_be_forcetranslation = os.path.split(path)
+    path, should_be_ckanext = os.path.split(path)
+
+    assert should_be_i18n_path == i18n_path
+    assert should_be_forcetranslation == 'forcetranslation'
+    assert should_be_ckanext == 'ckanext'
+    assert p.i18n_domain() == domain
